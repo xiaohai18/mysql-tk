8e98831d95985bb66f7d87653bfe443120475d46
unknown <mrhainer@qq.com>
2018-08-23 17:43:33 +0800

    修改package.json

--- test.js
+++ test.js
@@ -1,64 +1,61 @@
-var MysqlTK = require('./index');
+	var MysqlTK = require('./index');
 
-//Setting configuration
-MysqlTK.CONFIG = {
-	host     : 'localhost',
-	user     : 'root',
-	password : '',
-	database : 'dbName'
-};
+	//Setting configuration
+	MysqlTK.CONFIG = {
+		host     : 'localhost',
+		user     : 'root',
+		password : '',
+		database : 'dbName'
+	};
 
-//example
-MysqlTK('tableName')
-	.count((count)=>{
-		console.log(count);
-	});
+	//example
+	MysqlTK('tableName')
+		.count((count)=>{
+			console.log(count);
+		});
 
-MysqlTK('tableName')
-	.where({id:1,type:2})
-	.find((data)=>{
-		console.log(data);
-	});
+	MysqlTK('tableName')
+		.where({id:1,type:2})
+		.find((data)=>{
+			console.log(data);
+		});
 
-MysqlTK('tableName')
-	.fields(['id','name'])
-	.where({id:1,type:2})
-	.select((data)=>{
-		console.log(data);
-	});
+	MysqlTK('tableName')
+		.fields(['id','name'])
+		.where({id:1,type:2})
+		.select((data)=>{
+			console.log(data);
+		});
 
-MysqlTK('tableName')
-	.fields(['id','name'])
-	.where({id:1,type:2})
-	.limit([1,10], (data)=>{
-		console.log(data);
-	});
+	MysqlTK('tableName')
+		.fields(['id','name'])
+		.where({id:1,type:2})
+		.limit([1,10], (data)=>{
+			console.log(data);
+		});
 
-MysqlTK('tableName')
-	.query('select * from tableName', (data)=>{
-		console.log(data);
-	});
+	MysqlTK('tableName')
+		.query('select * from tableName', (data)=>{
+			console.log(data);
+		});
 
 
+	MysqlTK('tableName')
+		.add({field1: 'value1', field2: 'value2'}, (res)=>{
+			console.log(res);
+		});
 
-///----add
+	//----update
+	MysqlTK('tableName')
+		.where('id', 1)
+		.update({field1: 'value1', field2: 'value2'}, (res)=>{
+			console.log(res);
+		});
 
-MysqlTK('tableName')
-	.add({field1: 'value1', field2: 'value2'}, (res)=>{
-		console.log(res);
-	});
-
-//----update
-MysqlTK('tableName')
-	.where('id', 1)
-	.update({field1: 'value1', field2: 'value2'}, (res)=>{
-		console.log(res);
-	});
-
-//----delete
-MysqlTK('tableName')
-	.where('id', 1)
-	.delete((res)=>{
-		console.log(res);
-	});
+	//----delete
+	MysqlTK('tableName')
+		.where('id', 1)
+		.delete((res)=>{
+			console.log(res);
+		});
 
